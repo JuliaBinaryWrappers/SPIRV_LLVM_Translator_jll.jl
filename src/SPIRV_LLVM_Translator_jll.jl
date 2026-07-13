05a7683ee1c14ea123616f144aba3e6fab3de332
jlbuild <juliabuildbot@gmail.com>
2026-07-13 14:56:13 +0000

    SPIRV_LLVM_Translator_jll build 22.1.4+0

--- src/SPIRV_LLVM_Translator_jll.jl
+++ src/SPIRV_LLVM_Translator_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("SPIRV_LLVM_Translator")
-JLLWrappers.@generate_main_file("SPIRV_LLVM_Translator", UUID("4a5d46fc-d8cf-5151-a261-86b458210efb"))
+JLLWrappers.@generate_main_file("SPIRV_LLVM_Translator", Base.UUID("4a5d46fc-d8cf-5151-a261-86b458210efb"))
 end  # module SPIRV_LLVM_Translator_jll
